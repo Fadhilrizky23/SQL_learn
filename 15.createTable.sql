321942a683b91f24b1da038af3e0c873771a7067
Fadhilrizky23 <fadhilrizky231@gmail.com>
2026-06-05 00:02:01 +0700

    4 Juni 2026

---
+++ 15.createTable.sql
@@ -0,0 +1,6 @@
+Create table Database (
+id Integer PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+Version Float,
+Download_count Integer
+)
---
+++ 16.AlterTable.sql
@@ -0,0 +1,3 @@
+ALTER TABLE Movies ADD column Aspect_ratio Float 
+
+ALTER TABLE MOVIES ADD column Language Text DEFAULT English
